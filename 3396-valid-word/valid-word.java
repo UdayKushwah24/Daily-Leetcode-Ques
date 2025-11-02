2bfb518be44869e3943aff1dda695a49791b94d3
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-02 19:09:59 +0530

    Time: 1 ms (99.27%) | Memory: 43.3 MB (6.26%) - LeetSync

---
+++ 3396-valid-word/valid-word.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+        boolean isLower = false;
+        boolean isUpper = false; 
+        boolean iscon = false;
+        for(int i = 0;  i < word.length();i++) {
+            int ch = word.charAt(i);
+            if(ch == 97 || ch == 101 || ch == 105 || ch == 111 || ch == 117){
+                isLower = true;
+            }
+            else if(ch == 65 || ch == 69 || ch == 73 || ch == 79 || ch == 85){
+                isUpper = true;
+            }
+            else if((ch >= 48 && ch <= 57)) continue;
+            
+            else if((ch >= 97 && ch <= 122) ||( ch >= 65 && ch <= 90))
+            {   iscon = true;
+                 continue; }
+            else return false;
+
+
+        }
+
+        return (isLower || isUpper) && iscon;
+
+    }
+}
