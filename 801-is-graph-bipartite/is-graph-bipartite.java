4c05d0a906d5f8e15cfd89b02b56bde2ab8bc064
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-04 19:15:23 +0530

    Time: 8 ms (7.14%) | Memory: 47.7 MB (6.46%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -11,37 +11,35 @@ class Solution {
     }
 
     public boolean isBipartite(int[][] graph) {
-         Queue<BipartitePair> q = new LinkedList<>();
+        Queue<BipartitePair> q = new LinkedList<>();
         HashMap<Integer, Integer> visited = new HashMap<>();
         for (int i = 0; i < graph.length; i++) {
             if (visited.containsKey(i)) {
                 continue;
             }
-                q.add(new BipartitePair(i, 0));
-                while (!q.isEmpty()) {
-                    // 1. remove
-                    BipartitePair rp = q.poll();
-                    // 2 Ignore if already visited
-                    if (visited.containsKey(rp.vtx)) {
-                        if (visited.get(rp.vtx) != rp.dis) {
-                            return false;
-                        }
-                        continue;
+            q.add(new BipartitePair(i, 0));
+            while (!q.isEmpty()) {
+                // 1. remove
+                BipartitePair rp = q.poll();
+                // 2 Ignore if already visited
+                if (visited.containsKey(rp.vtx)) {
+                    if (visited.get(rp.vtx) != rp.dis) {
+                        return false;
                     }
-                    // 3. Marked visited
-                    visited.put(rp.vtx, rp.dis);
-                    // 4. self work
+                    continue;
+                }
+                // 3. Marked visited
+                visited.put(rp.vtx, rp.dis);
+                // 4. self work
 
-
-                    // 5. Add unvisited nbsrs
-                    for (int nbrs : graph[rp.vtx]) {
-                        if (!visited.containsKey(nbrs)) {
-                            q.add(new BipartitePair(nbrs, rp.dis + 1));
-                        }
+                // 5. Add unvisited nbsrs
+                for (int nbrs : graph[rp.vtx]) {
+                    if (!visited.containsKey(nbrs)) {
+                        q.add(new BipartitePair(nbrs, rp.dis + 1));
                     }
                 }
             }
-        
+        }
 
         return true;
     }
