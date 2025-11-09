84733c443937c9b1450034827801c075160727b5
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-09 11:35:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (12.55%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+
+
+    
+    public static int rob(int [] dp,int[] nums, int i) {
+        if(i == nums.length || i == nums.length+1) {
+            return 0;
+        }
+        if(dp[i] != -1) return dp[i];
+        int rob = nums[i] + rob(dp,nums, i+2);
+        int notrob = rob(dp,nums, i+1);
+        return dp[i] = Math.max(rob, notrob);
+    }
+    public int rob(int[] arr) {
+         int dp[] = new int[arr.length];
+        Arrays.fill(dp, -1);
+        return rob(dp,arr, 0);
+    }
+}
