3ea958bec911a9ea3d2136f7057e81354e2ed21a
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-14 11:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (14.68%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,46 +9,46 @@
  * }
  */
 class Solution {
-    /*
-     * public ListNode reverseList(ListNode head) {
-     * ListNode curr = head;
-     * ListNode prev = null;
-     * while(curr != null) {
-     * ListNode ahead = curr.next;
-     * curr.next = prev;
-     * prev = curr;
-     * curr = ahead;
-     * }
-     * return prev;
-     * }
-     */
+    
+       public ListNode reverseList(ListNode head) {
+       ListNode curr = head;
+       ListNode prev = null;
+       while(curr != null) {
+       ListNode ahead = curr.next;
+       curr.next = prev;
+       prev = curr;
+       curr = ahead;
+       }
+       return prev;
+       }
+    
 
-    /*
-     * public ListNode reverseList(ListNode head) {
-     * ListNode first = head;
-     * ListNode prev = null;
-     * ListNode curr = head;
-     * while(curr.next != null){
-     * LisNode temp = head;
-     * curr.next = prev;
-     * prev = curr.next;
-     * curr = temp.next;
-     * }
-     * }
-     */
+    
+    //    public ListNode reverseList(ListNode head) {
+    //         ListNode first = head;
+    //         ListNode prev = null;
+    //         ListNode curr = head;
+    //         while(curr.next != null){
+    //             LisNode temp = head;
+    //             curr.next = prev;
+    //             prev = curr.next;
+    //             curr = temp.next;
+    //         }
+    //    }
+    
 
     // Using Recursion :
-    public ListNode reverseList(ListNode head) {
+    /* public ListNode reverseList(ListNode head) {
         return reverse(head, null);
     }
 
     public ListNode reverse(ListNode curr, ListNode prev) {
         if (curr == null) {
             return prev;
-        }
+        }.
         ListNode temp = reverse(curr.next, curr);
         curr.next = prev;
         
         return temp;
-    }
+    } */
 }
