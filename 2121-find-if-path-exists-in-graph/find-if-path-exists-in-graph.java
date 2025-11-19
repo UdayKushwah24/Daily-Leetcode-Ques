90e7c1cef999a8dffa359180f32072914f30b764
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-20 00:01:33 +0530

    Time: 123 ms (64.46%) | Memory: 274.8 MB (22.84%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int pair[] : edges) {
+            int u = pair[0];
+            int v = pair[1];
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+        return dfs(source,destination,adjList);
+    }
+
+    public boolean dfs(int src, int des, ArrayList<ArrayList<Integer>> adjList) {
+        Stack<Integer> st = new Stack<>();
+        HashSet<Integer> visited = new HashSet<>();
+        st.push(src);
+        while (!st.isEmpty()) {
+            //1.remove
+            int r = st.pop();
+            //Ignore 
+            if (visited.contains(r)) {
+                continue;
+            }
+            //mark visited
+            visited.add(r);
+            //self work
+            if(r==des){
+                return true;
+            }
+            
+            //add unvisited nbrs
+            for (int nbrs : adjList.get(r)) {
+                if (!visited.contains(nbrs)) 
+                    st.push(nbrs);
+                }
+            }
+            return false;
+            
+        }
+}
