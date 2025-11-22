526605c1c3c1c254c17fc27b414f9ed2fc7c9e31
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-22 08:54:15 +0530

    Time: 1 ms (19.52%) | Memory: 43.6 MB (18.77%) - LeetSync

--- 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
@@ -1,22 +1,4 @@
-/* class Solution {
-    public int minimumOperations(int[] arr) {
-        int count = 0;
-         for (int i = 0; i < arr.length; i++) {
-            while(arr[i]%3 != 0) {
-                if(arr[i]%3 == 1) {
-                    arr[i]--;
-                    count++;
-                } else {
-                    arr[i]++;
-                    count++;
-                }
-            }
-        }
-       
-        return count;
-    }
-} */
-
+ 
 class Solution {
     public int minimumOperations(int[] arr) {
         int count = 0;
