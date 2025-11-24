765d0d9ee46431a5f67875fcb119e5a32dc762e5
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-11-24 23:52:57 +0530

    Time: 10 ms (16.13%) | Memory: 47.5 MB (5.92%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i < numCourses; i++) {
+            map.put(i , new ArrayList<>());
+        }
+        for(int i = 0; i < prerequisites.length; i++) {
+            int v1 = prerequisites[i][0];
+            int v2 = prerequisites[i][1];
+            map.get(v2).add(v1);
+        }
+        int in[] = new int[numCourses];
+        for(int vtx : map.keySet()) {
+            for(int nbrs : map.get(vtx)) {
+                in[nbrs]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++) {
+            if(in[i] == 0) {
+                q.add(i);
+            }
+        }
+        int c = 0; 
+        while(!q.isEmpty()) {
+            int v = q.poll();
+            c++;
+            for(int nbrs : map.get(v)) {
+                in[nbrs]--;
+                if(in[nbrs] == 0) {
+                    q.add(nbrs);
+                }
+            }
+        }
+        return numCourses == c;
+    }
+}
