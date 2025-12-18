c850e6dba093d5db5353bc2bc2ddc9814712ab17
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-12-19 00:39:25 +0530

    Time: 118 ms (6.08%) | Memory: 81.6 MB (8.20%) - LeetSync

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/minimized-maximum-of-products-distributed-to-any-store.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimizedMaximum(int n, int[] quantities) {
+        int l = 1;
+        int r = Arrays.stream(quantities).max().getAsInt();
+        int ans = Integer.MIN_VALUE;
+        while(l <= r) {
+            int mid = l + (r - l)/2;
+            if(possible(quantities, n, mid)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean possible(int[ ] arr, int threshold, int mid) {
+        int div = 0;
+        for(int i = 0;  i < arr.length; i++) {
+            div += Math.ceil(arr[i]*1.0/mid);
+        }
+        return div <= threshold;
+    }
+}
