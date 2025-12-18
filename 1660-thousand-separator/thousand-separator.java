35e86fb0ce81665ae0a7954937f930537386f7f4
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-12-18 14:21:27 +0530

    Time: 1 ms (30.59%) | Memory: 42.7 MB (21.47%) - LeetSync

---
+++ 1660-thousand-separator/thousand-separator.java
@@ -0,0 +1,35 @@
+// class Solution {
+//     public String thousandSeparator(int n) {
+//         StringBuilder sb = new StringBuilder();
+//         String s = n + "";
+//         int c = 0;
+//         for(int i = s.length()-1; i >= 0 ; i--) {
+//             c++;
+//             sb.append(0, s.charAt(i));
+//             if(i != 0 && c == 3) {
+//                 sb.append(0, ".");
+//                 c = 0;
+//             }
+//         }
+//         return sb.toString();
+//     }
+// }
+
+class Solution {
+    public String thousandSeparator(int n) {
+        StringBuilder sb = new StringBuilder();
+        String s = n + "";
+        int c = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            c++;
+            sb.insert(0, s.charAt(i));
+
+            if(c == 3 && i != 0) {    
+                sb.insert(0, ".");
+                c = 0;
+            }
+        }
+        return sb.toString();
+    }
+}
