a8630f0e65eab481cb9a2f2c2c7cc44ca9dcb014
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2025-12-30 11:36:18 +0530

    Time: 3 ms (86.74%) | Memory: 52.4 MB (30.68%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -4,13 +4,13 @@ class Solution {
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || grid[i][j] == '*') {
             return;
         }
- 
-            grid[i][j] = '*'; // mark visited
-            dfs(grid, i, j - 1); // left
-            dfs(grid, i - 1, j); // top
-            dfs(grid, i, j + 1); // right
-            dfs(grid, i + 1, j); // bottom
-        
+
+        grid[i][j] = '*'; // mark visited
+        dfs(grid, i, j - 1); // left
+        dfs(grid, i - 1, j); // top
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i + 1, j); // bottom
+
     }
 
     public int numIslands(char[][] grid) {
