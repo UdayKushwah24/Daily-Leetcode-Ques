b18e757af96271ce7ba824c13f405faef934d597
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-13 12:36:09 +0530

    Time: 14 ms (98.28%) | Memory: 45.7 MB (17.05%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,4 +1,4 @@
- 
+/*  
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
@@ -23,23 +23,78 @@ class Solution {
 
         return result;
     }
-
-
-
 }
 
+ */
+
+  
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         Arrays.sort(nums);
+//         int n = nums.length;
+//         int resultLeft = Integer.MIN_VALUE;
+//         int resultRight = Integer.MAX_VALUE;
+//         for (int i = 0; i < n ; i++) {
+//             int left = i + 1, right = n - 1;
+
+//             while (left < right) {
+//                 int sum = nums[i] + nums[left] + nums[right];
+//                 if(sum < target) {
+//                     resultLeft = Math.max(sum, resultLeft);
+//                 }
+//                 if(sum > target) {
+//                     resultRight = Math.min(sum, resultRight);
+//                 }
+
+//                 if (sum == target) return target;
+//                 else if (sum < target) left++;
+//                 else right--;
+//             }
+//         }
+//         if(resultLeft - target < resultRight - target) {
+//             return resultLeft;
+//         } else {
+//             return resultRight;
+//         }
+ 
+//     }
+// }
 
 
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
 
+        int resultLeft = Integer.MIN_VALUE;
+        int resultRight = Integer.MAX_VALUE;
 
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
 
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
 
+                if (sum == target) return target;
 
+                if (sum < target) {
+                    resultLeft = Math.max(resultLeft, sum);
+                    left++;
+                } else {
+                    resultRight = Math.min(resultRight, sum);
+                    right--;
+                }
+            }
+        }
 
+        if (resultLeft == Integer.MIN_VALUE) return resultRight;
+        if (resultRight == Integer.MAX_VALUE) return resultLeft;
 
-
-
-
-
-
+        if (Math.abs(target - resultLeft) <= Math.abs(target - resultRight)) {
+            return resultLeft;
+        } else {
+            return resultRight;
+        }
+    }
+}
 
