1dce62f2be1ea6e041c4d7e880ef72b9b367dc53
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-15 23:04:28 +0530

    Time: 1 ms (37.40%) | Memory: 42.5 MB (7.12%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 
     public int integerReplacement(int n) {
         return solve((long) n); 
@@ -18,5 +18,32 @@ class Solution {
                     solve(n + 1)
             );                          
         }
+    }
+}
+ */
+
+ class Solution {
+
+    Map<Long, Integer> dp = new HashMap<>();
+
+    public int integerReplacement(int n) {
+        return solve((long) n);
+    }
+
+    private int solve(long n) {
+
+        if (n == 1) return 0;
+
+        if (dp.containsKey(n)) return dp.get(n);
+
+        int ans;
+        if (n % 2 == 0) {
+            ans = 1 + solve(n / 2);
+        } else {
+            ans = 1 + Math.min(solve(n - 1), solve(n + 1));
+        }
+
+        dp.put(n, ans);
+        return ans;
     }
 }
