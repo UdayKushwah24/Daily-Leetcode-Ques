33cdf9bad0bd2b052e8fa7f4415bbeb63f340f73
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-15 23:02:01 +0530

    Time: 7 ms (9.38%) | Memory: 41.9 MB (79.10%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,22 +1,22 @@
 class Solution {
 
     public int integerReplacement(int n) {
-        return solve((long) n);   // small change to avoid overflow
+        return solve((long) n); 
     }
 
     public int solve(long n) {
 
         if (n == 1) {
-            return 0;            // FIX 1: base case
+            return 0;             
         }
 
         if (n % 2 == 0) {
-            return 1 + solve(n / 2);   // FIX 2: add 1 step
+            return 1 + solve(n / 2);   
         } else {
             return 1 + Math.min(
                     solve(n - 1),
                     solve(n + 1)
-            );                          // FIX 3: correct logic
+            );                          
         }
     }
 }
