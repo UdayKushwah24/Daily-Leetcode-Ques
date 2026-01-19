03c1ec213dd53ec74322659738aa16c1eacb3221
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-19 10:28:24 +0530

    Time: 3 ms (63.28%) | Memory: 44 MB (23.09%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++ ) {
+            char ch = s.charAt(i);
+            if(!stack.isEmpty() && stack.peek()+32 == ch) {
+                stack.pop();
+            } 
+            else if(!stack.isEmpty() && stack.peek() == ch+32) {
+                stack.pop();
+            }  else {
+                stack.push(ch);
+            }
+
+        }
+        StringBuilder sb = new StringBuilder();
+        while(stack.size() > 0) {
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
