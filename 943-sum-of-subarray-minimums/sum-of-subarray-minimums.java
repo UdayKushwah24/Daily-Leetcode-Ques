8ddb44f814824c153ab0f015aab30d38d1f2e97d
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-19 11:54:15 +0530

    Time: 62 ms (59.97%) | Memory: 59.7 MB (8.02%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        long mod = 1000000007;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+
+        // Previous Less Element
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? i + 1 : i - st.peek();
+            st.push(i);
+        }
+
+        st.clear();
+
+        // Next Less or Equal Element
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n - i : st.peek() - i;
+            st.push(i);
+        }
+
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = (ans + (long) arr[i] * left[i] * right[i]) % mod;
+        }
+
+        return (int) ans;
+    }
+}
