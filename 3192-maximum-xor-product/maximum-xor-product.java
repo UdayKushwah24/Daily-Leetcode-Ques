95fbd93a77268c71a3c0c1bb119b9759cae1c698
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-21 22:07:24 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (18.37%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.java
+++ 3192-maximum-xor-product/maximum-xor-product.java
@@ -7,10 +7,10 @@ class Solution {
         long setBit = 1;
         for (int i = 49; i >= n; i--) {
             if ((1 & (a >> i)) == 1) {
-                axorx = (axorx ^ (setBit << i)) ;
+                axorx = (axorx | (setBit << i)) ;
             }
             if ((1 & (b >> i)) == 1) {
-                bxorx = (bxorx ^ (setBit << i)) ;
+                bxorx = (bxorx | (setBit << i)) ;
             }
         }
         for (
@@ -18,13 +18,13 @@ class Solution {
             int abit = (int) (1 & (a >> i));
             int bbit = (int) (1 & (b >> i));
             if (abit == bbit) {
-                axorx = (axorx ^ (setBit << i)) ;
-                bxorx = (bxorx ^ (setBit << i)) ;
+                axorx = (axorx | (setBit << i)) ;
+                bxorx = (bxorx | (setBit << i)) ;
             } else {
                 if (axorx < bxorx) {
-                    axorx = (axorx ^ (setBit << i));
+                    axorx = (axorx | (setBit << i));
                 } else {
-                    bxorx = (bxorx ^ (setBit << i)) ;
+                    bxorx = (bxorx | (setBit << i)) ;
 
                 }
             }
