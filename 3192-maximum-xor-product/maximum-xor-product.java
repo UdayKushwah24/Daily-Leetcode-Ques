040f1ced05e1bbfbbfd38af195ac00307e975097
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-21 22:10:50 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (8.16%) - LeetSync

--- 3192-maximum-xor-product/maximum-xor-product.java
+++ 3192-maximum-xor-product/maximum-xor-product.java
@@ -22,7 +22,7 @@ class Solution {
                 bxorx = (bxorx | (setBit << i)) ;
             } else {
                 if (axorx < bxorx) {
-                    axorx = (axorx | (setBit << i));
+                    axorx = (axorx | (setBit << i));   
                 } else {
                     bxorx = (bxorx | (setBit << i)) ;
 
@@ -34,3 +34,42 @@ class Solution {
         // System.out.println();
         return (int) (((axorx % mod)*(bxorx% mod)) % mod);}
 }
+
+
+/* 
+
+
+public static int maximumXorProduct(long a, long b, int n) {
+        long mod = 1000000007;
+        long axorx = 0;
+        long bxorx = 0;
+        long setBit = 1;
+        for (int i = 49; i >= n; i--) {
+            if ((1 & (a >> i)) == 1) {
+                axorx = (axorx ^ (setBit << i)) ;
+            }
+            if ((1 & (b >> i)) == 1) {
+                bxorx = (bxorx ^ (setBit << i)) ;
+            }
+        }
+        for (
+            int i = n - 1; i >= 0; i--) {
+            int abit = (int) (1 & (a >> i));
+            int bbit = (int) (1 & (b >> i));
+            if (abit == bbit) {
+                axorx = (axorx ^ (setBit << i)) ;
+                bxorx = (bxorx ^ (setBit << i)) ;
+            } else {
+                if (axorx < bxorx) {
+                    axorx = (axorx ^ (setBit << i));
+                } else {
+                    bxorx = (bxorx ^ (setBit << i)) ;
+
+                }
+            }
+        }
+        // System.out.println(axorx + " " + bxorx);
+        // System.out.println(axorx*bxorx);
+        // System.out.println();
+        return (int) (((axorx % mod)*(bxorx% mod)) % mod);
+    } */
