5eb29df6c1befd6f434f835c8f5e02e09da36d80
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-22 22:04:39 +0530

    Time: 3 ms (86.67%) | Memory: 43.5 MB (80.76%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public static boolean checkDuplicate(String str) {
         HashSet<Character> set = new HashSet<>();
         for (char ch : str.toCharArray()) {
@@ -44,6 +44,63 @@ class Solution {
     
     static int result = 0;
     private static void solve(int idx, int temp, int[] arr) {
+        // result = Math.max(result, Integer.bitCount(temp));
+        result = Math.max(result, countSetbit(temp));
+        for (int i = idx; i < arr.length; i++) {
+            if ((temp & arr[i]) == 0) {
+                solve(i + 1, temp | arr[i], arr);
+            }
+        }
+    }
+} */
+
+
+class Solution {
+    public  boolean checkDuplicate(String str) {
+        HashSet<Character> set = new HashSet<>();
+        for (char ch : str.toCharArray()) {
+            set.add(ch);
+        }
+        return set.size() == str.length();
+    }
+
+    public  int SetBit(String str) {
+        int num = 0;
+        for (char ch : str.toCharArray()) {
+            num |= (1 << (ch - 'a'));
+        }
+        return num;
+    }
+
+    public  int maxLength(List<String> arr) {
+        List<String> list = new ArrayList<>();
+        for (String str : arr) {
+            if (checkDuplicate(str)) {
+                list.add(str);
+            }
+        }
+        int[] array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = SetBit(list.get(i));
+        }
+        
+        int temp = 0;
+        solve(0, temp, array);
+        
+        return result;
+    }
+
+    public  int countSetbit(int n) {
+        int set = 0;
+        while(n > 0) {
+            set++;
+            n = (n & (n-1));
+        }
+        return set;
+    }
+    
+     int result = 0;
+    private  void solve(int idx, int temp, int[] arr) {
         // result = Math.max(result, Integer.bitCount(temp));
         result = Math.max(result, countSetbit(temp));
         for (int i = idx; i < arr.length; i++) {
