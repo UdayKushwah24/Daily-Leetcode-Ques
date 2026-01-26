3ff9fd1bbc3f0ce2b24a901c104ecb4d1373623f
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-26 21:29:29 +0530

    Time: 16 ms (96.20%) | Memory: 49.9 MB (10.89%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,9 +1,19 @@
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int ans = 0;
+//         for(int i = 0; i < nums.length-1; i+=2) {
+//             ans += Math.min(nums[i], nums[i+1]);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
         int ans = 0;
-        for(int i = 0; i < nums.length-1; i+=2) {
-            ans += Math.min(nums[i], nums[i+1]);
+        for(int i = 0; i < nums.length; i+=2) {
+            ans += nums[i];
         }
         return ans;
     }
