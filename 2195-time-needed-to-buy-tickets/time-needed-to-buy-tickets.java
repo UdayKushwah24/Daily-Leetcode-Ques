cdd3391ad66495af4804a1c8d09b93cb7fe12878
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-28 12:43:29 +0530

    Time: 12 ms (8.48%) | Memory: 46.5 MB (5.29%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -1,12 +1,14 @@
-/* class Solution {
+class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
         Queue<Integer> q = new LinkedList<>();
         int kthPerson = tickets[k];
         for(int i = 0; i < tickets.length; i++) {
             if(i == k) {
                 q.add(-1);
+            } else {
+
+                q.add(tickets[i]);
             }
-            q.add(tickets[i]);
         }
         int sec = 0;
 
@@ -21,7 +23,7 @@
                     return sec;
                 }
                  
-                
+                continue;
             }
             
             
@@ -33,42 +35,41 @@
         }
         return 0;
     }
-} */
+} 
 
 
-class Solution {
-    public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Integer> q = new LinkedList<>();
-        int kthPerson = tickets[k];
-        for(int i = 0; i < tickets.length; i++) {
-            if(i == k) {
-                q.add(-1);
-            } else {
+// class Solution {
+//     public int timeRequiredToBuy(int[] tickets, int k) {
+//         Queue<Integer> q = new LinkedList<>();
+//         int kthPerson = tickets[k];
+//         for(int i = 0; i < tickets.length; i++) {
+//             if(i == k) {
+//                 q.add(-1);
+//             } else {
 
-            q.add(tickets[i]);
-            }
-        }
-        int sec = 0;
+//             q.add(tickets[i]);
+//             }
+//         }
+//         int sec = 0;
 
-        while(!q.isEmpty()) {
-            
-            sec++;
-            int first = q.poll();
-            if(first == -1) {
-                kthPerson--;
-                q.add(-1);
-                if(kthPerson == 0) {
-                    return sec;
-                }  
-            }            
-           else {
-                first = first - 1;
-                if(first > 0)
-                q.add(first);
-            }
+//         while(!q.isEmpty()) {
+//             sec++;
+//             int first = q.poll();
+//             if(first == -1) {
+//                 kthPerson--;
+//                 q.add(-1);
+//                 if(kthPerson == 0) {
+//                     return sec;
+//                 }  
+//             }            
+//            else {
+//                 first = first - 1;
+//                 if(first > 0)
+//                 q.add(first);
+//             }
 
-        }
-        return 0;
+//         }
+//         return 0;
    
-    }
-}
+//     }
+// }
