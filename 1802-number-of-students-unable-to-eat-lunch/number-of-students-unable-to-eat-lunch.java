2697f4ef89391db9945cc2a243cbd4af4a7d2564
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-28 13:39:32 +0530

    Time: 2 ms (43.30%) | Memory: 43.4 MB (47.82%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
         Queue<Integer> student = new LinkedList<>();
-        //Stack<Integer> sandwich = new Stack<Integer>();
         int count0 = 0;
         int count1 = 0;
         for (int a : students) {
@@ -29,8 +28,42 @@ class Solution {
                 int rv = student.poll();
                 student.add(rv);
             }
-            
+
         }
         return student.size();
     }
 }
+
+/*
+
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        Queue<Integer> q = new LinkedList<>();
+        int c0 = 0, c1 = 0;
+        for (int s : students) {
+            q.add(s);
+            if (s == 0) c0++;
+            else c1++;
+        }
+
+        int i = 0;
+        while (i < sandwiches.length) {
+            int sw = sandwiches[i];
+            if (sw == 0 && c0 == 0) break;
+            if (sw == 1 && c1 == 0) break;
+            if (q.peek() == sw) {
+                q.poll();
+                i++;
+                if (sw == 0) c0--;
+                else c1--;
+            } else {
+                q.add(q.poll());
+            }
+        }
+        return q.size();
+    }
+}
+
+
+
+ */
