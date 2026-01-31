743e698811434edfca55a8c4f53103a0aacda8a3
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-01-31 19:37:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (13.54%) - LeetSync

--- 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.java
+++ 1303-minimum-moves-to-reach-target-score/minimum-moves-to-reach-target-score.java
@@ -1,10 +1,29 @@
-class Solution {
+/* class Solution {
     public int minMoves(int target, int maxDoubles) {
         int ans = 0;
         while (target > 1 && maxDoubles > 0) {
             if(target % 2 == 0) {
                 maxDoubles--;
                 target /= 2;
+            } else {
+                target--;
+            }
+            ans++;
+        }
+        ans += target-1;
+        return ans;
+
+    }
+} */
+
+
+class Solution {
+    public int minMoves(int target, int maxDoubles) {
+        int ans = 0;
+        while (target > 1 && maxDoubles > 0) {
+            if((target & 1) == 0) {
+                maxDoubles--;
+                target >>= 1;
             } else {
                 target--;
             }
