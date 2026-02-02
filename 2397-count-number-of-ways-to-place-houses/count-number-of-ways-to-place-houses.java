cb058c520723e4782f812108a90b8ee9e00ef1ff
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-02 22:06:20 +0530

    Time: 13 ms (16.58%) | Memory: 46 MB (29.95%) - LeetSync

---
+++ 2397-count-number-of-ways-to-place-houses/count-number-of-ways-to-place-houses.java
@@ -0,0 +1,23 @@
+class Solution {
+    long mod = 1000000007;
+    public int countHousePlacements(int n) {
+        long dp[] = new long[n+1];
+        Arrays.fill(dp, -1);
+        long x = placeHouse(dp, n, 0) ;
+         
+        return (int)((x * x)%mod);
+    }
+
+
+    private   long placeHouse(long[] dp, int n, int i) {
+        if(i >= n) {
+            return 1;
+        }
+        if(dp[i] != -1) return dp[i];
+         
+
+        long place_House = placeHouse(dp, n, i+2) % mod;
+        long notPlaceHouse = placeHouse(dp , n, i+1) % mod;
+        return dp[i] =( place_House + notPlaceHouse) % mod;
+    }
+}
