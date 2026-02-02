7dbfeeff19421b69a19320ad4c3e5436c73874cf
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-02 12:13:56 +0530

    Time: 1 ms (99.88%) | Memory: 44.1 MB (94.35%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -45,7 +45,7 @@
 
 
 
-
+/* 
 class Solution {
     public int minimumPairRemoval(int[] nums) {
         int n = nums.length;
@@ -63,6 +63,42 @@ class Solution {
             }
             nums[pos-1] = minSum;
             for(int i = pos;i < n-1;i++) {
+                nums[i] = nums[i+1];
+            }
+            n--;
+        }
+        return ops;
+    }
+
+    private boolean isSorted(int[] arr, int n) {
+        for (int i = 1; i < n ; i++) {
+            if (arr[i] < arr[i - 1])
+                return false;
+        }
+        return true;
+    }
+} */
+
+
+
+
+class Solution {
+    public int minimumPairRemoval(int[] nums) {
+        int n = nums.length;
+        int ops = 0;
+        while (!isSorted(nums, n)) {
+            ops++;
+            int minSum = Integer.MAX_VALUE;
+            int pos = -1;
+            for (int i = 0; i < n-1 ; i++) {
+                int sum = nums[i] + nums[i + 1];
+                if (sum < minSum) {
+                    minSum = sum;
+                    pos = i;
+                }
+            }
+            nums[pos] = minSum;
+            for(int i = pos+1;i < n-1;i++) {
                 nums[i] = nums[i+1];
             }
             n--;
