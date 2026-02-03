01d4359d0d8addea725293d76ff6922663d8c069
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-03 19:43:54 +0530

    Time: 656 ms (69.08%) | Memory: 48.6 MB (69.88%) - LeetSync

---
+++ 2108-minimize-the-difference-between-target-and-chosen-elements/minimize-the-difference-between-target-and-chosen-elements.java
@@ -0,0 +1,47 @@
+class Solution {
+
+    int ans;
+    int tgt;
+    int[][] dp;
+
+    public int minimizeTheDifference(int[][] mat, int target) {
+        ans = Integer.MAX_VALUE;
+        tgt = target;
+
+        int m = mat.length;
+        int maxSum = 5000; // constraints based
+
+        dp = new int[m][maxSum + 1];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        int n = mat[0].length;
+        for (int col = 0; col < n; col++) {
+            solve(mat, 0, 0, col);
+        }
+        return ans;
+    }
+
+    public void solve(int[][] mat, int sum, int cr, int cc) {
+
+        sum += mat[cr][cc];
+
+        if (sum > 5000) return;   // pruning
+
+        if (cr == mat.length - 1) {
+            ans = Math.min(ans, Math.abs(tgt - sum));
+            return;
+        }
+
+        if (dp[cr][sum] != -1 && dp[cr][sum] <= Math.abs(tgt - sum)) {
+            return; // already explored better
+        }
+
+        dp[cr][sum] = Math.abs(tgt - sum);
+
+        for (int i = 0; i < mat[0].length; i++) {
+            solve(mat, sum, cr + 1, i);
+        }
+    }
+}
