53a39590df3c30c37376568c7dd8b4407ca7b845
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-05 22:40:45 +0530

    Time: 36 ms (33.33%) | Memory: 184.5 MB (48.81%) - LeetSync

--- 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
@@ -1,34 +1,65 @@
 class Solution {
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+
         int n = nums.length;
-        int a[] = new int[101];
-        int res[] = new int[n - k + 1];
-        for (int i=0;i<k;i++)
-        {
-            a[nums[i]+50]++;
+        int[] a = new int[101];
+        int[] res = new int[n - k + 1];
+
+        // first window
+        for (int j = 0; j < k; j++) {
+            a[nums[j] + 50]++;
         }
-        for (int i=0;i<=n-k;i++)
-        {
-            int count=0,val=-1;
-            for(int j=0;j<=49;j++)
-            {
-                if(a[j]>0)
-                {
-                    count+=a[j];
-                    if(count>=x)
-                    {
-                        val=j;
-                        break;
-                    }
+
+        for (int i = 0; i <= n - k; i++) {
+
+            int c = 0;
+
+            for (int p = 0; p < 101; p++) {
+                c += a[p];
+                if (c >= x) {
+                    int val = p - 50;
+                    res[i] = val < 0 ? val : 0;
+                    break;
                 }
             }
-            a[nums[i]+50]--;
-            if(i<n-k)
-            {
-                a[nums[i+k]+50]++;
+
+            // slide window
+            if (i + k < n) {
+                a[nums[i] + 50]--;        // remove left
+                a[nums[i + k] + 50]++;    // add right
             }
-            res[i] = val == -1 ? 0 : val-50;
         }
         return res;
     }
 }
+ /* 
+
+class Solution {
+    public int[] getSubarrayBeauty(int[] nums, int k, int x) {
+        int n = nums.length;
+        int a[] = new int[101];
+        int res[] = new int[n - k + 1];
+        for (int j = 0; j < k; j++) {
+            a[nums[j] + 50]++;
+        }
+        for (int i = 0; i < n - k; i++) {
+            
+            int c = 0;
+            for(int p = 0; p < 101; p++) {
+                if(a[p] != 0) {
+                    c+=a[p];
+                }
+                if(c >= x){
+                    int val = (p - 50);
+                    if(val < 0) res[i] = val;
+                    else res[i] = 0;
+
+                    a[p] = a[p] -1;
+                    break;
+                }
+            }
+            a[nums[i+k] + 50]++;
+        }
+        return res;
+    }
+} */
