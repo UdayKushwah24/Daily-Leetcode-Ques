576b56a020f170c65d24590d35146f80885f4b45
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-08 09:15:26 +0530

    Time: 1 ms (30.78%) | Memory: 46.6 MB (5.50%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/* class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+
+        
+        int hlst =  HeightTree(root.left);   // height left Sub-tree
+        int hrst =  HeightTree(root.right);   // height right Sub-tree
+        if(Math.abs(hlst- hrst) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int HeightTree(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int lh = HeightTree(node.left);
+        int rh = HeightTree(node.right);
+        return Math.max(lh, rh)+1;
+    }
+} */
+class Solution {
+     
+      class BalancePair {
+            boolean isbal = true;
+            int ht = -1;
+        }
+        
+
+        public boolean isBalanced(TreeNode root) {
+            return isBalance(root).isbal;      
+        }
+        public BalancePair isBalance(TreeNode root) {
+            if (root == null) {
+                return new BalancePair();
+            }
+            BalancePair lbp = isBalance(root.left);
+            BalancePair rbp = isBalance(root.right);
+            BalancePair sbp = new BalancePair();
+            sbp.ht = Math.max(lbp.ht, rbp.ht) + 1;
+            boolean sb = Math.abs(lbp.ht - rbp.ht) <= 1;
+            sbp.isbal = lbp.isbal && rbp.isbal && sb;
+            return sbp;
+      
+        }
+
+}
