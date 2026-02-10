81953462cc217f1b72df5cd734ce221eb2719684
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-10 23:59:47 +0530

    Time: 7 ms (51.40%) | Memory: 59.5 MB (73.85%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.java
@@ -12,28 +12,56 @@
  *         this.right = right;
  *     }
  * }
- */
+ *//* 
+    class Solution {
+     static final int MOD = 1_000_000_007;
+     long totalSum = 0;
+     long maxProduct = 0;
+    
+     long totalsum(TreeNode root) {
+         if (root == null) return 0;
+         return root.val 
+              + totalsum(root.left) 
+              + totalsum(root.right);
+     }
+    
+     long dfs(TreeNode root) {
+         if (root == null) return 0;
+    
+         long left = dfs(root.left);
+         long right = dfs(root.right);
+    
+          
+         long subSum = root.val + left + right;
+    
+         long product = subSum * (totalSum - subSum);
+         maxProduct = Math.max(maxProduct, product);
+    
+         return subSum;
+     }
+    
+     public int maxProduct(TreeNode root) {
+         totalSum = totalsum(root); // O(n)
+         dfs(root);                    // O(n)
+         return (int)(maxProduct % MOD);
+     }
+    } */
+
 class Solution {
     static final int MOD = 1_000_000_007;
-    long totalSum = 0;
+     
     long maxProduct = 0;
- 
-    public long totalsum(TreeNode root) {
-        if (root == null) return 0;
-        return root.val 
-             + totalsum(root.left) 
-             + totalsum(root.right);
-    }
- 
-    public long dfs(TreeNode root) {
-        if (root == null) return 0;
- 
-        long left = dfs(root.left);
-        long right = dfs(root.right);
+    long totalSum = 0;
 
-         
+    long totalsum(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        long left = totalsum(root.left);
+        long right = totalsum(root.right);
+
         long subSum = root.val + left + right;
- 
+
         long product = subSum * (totalSum - subSum);
         maxProduct = Math.max(maxProduct, product);
 
@@ -42,7 +70,8 @@ class Solution {
 
     public int maxProduct(TreeNode root) {
         totalSum = totalsum(root); // O(n)
-        dfs(root);                    // O(n)
-        return (int)(maxProduct % MOD);
+         totalsum(root); // O(n)
+        
+        return (int) (maxProduct % MOD);
     }
 }
