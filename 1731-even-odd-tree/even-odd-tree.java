234ecee30127b78b84b5fe4d168cbd2771cc5a69
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-10 14:20:31 +0530

    Time: 14 ms (26.86%) | Memory: 74.1 MB (96.58%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.java
+++ 1731-even-odd-tree/even-odd-tree.java
@@ -14,75 +14,74 @@
  * }
  */
 
-    class Solution {
-        public boolean isEvenOddTree(TreeNode root) {
-            return LevelOrderTraversal(root);
-        }
-
-        public boolean CheckIncreasing(List<Integer> ll) {
-            boolean flag = true;
-
-            if (ll.get(0) % 2 == 0) {
-                flag = false;
-            }
-            for (int i = 1; i < ll.size(); i++) {
-                if (ll.get(i - 1) >= ll.get(i) || ll.get(i) % 2 == 0) {
-                    flag = false;
-                    break;
-                }
-
-            }
-            return flag;
-        }
-
-        public boolean CheckDecreasing(List<Integer> ll) {
-            boolean flag = true;
-
-            if (ll.get(0) % 2 != 0) {
-                flag = false;
-            }
-            for (int i = 1; i < ll.size(); i++) {
-                if (ll.get(i - 1) <= ll.get(i) || ll.get(i) % 2 != 0) {
-                    flag = false;
-                    break;
-                }
-            }
-            return flag;
-        }
-
-        public boolean LevelOrderTraversal(TreeNode root) {
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.add(root);
-            boolean flag = true;
-
-            while (!queue.isEmpty()) {
-                List<Integer> ll = new ArrayList<>();
-                int size = queue.size();
-                
-
-                for (int i = 0; i < size; i++) {
-                    TreeNode rv = queue.remove();
-                    ll.add(rv.val);
-                    if (rv.left != null) {
-                        queue.add(rv.left);
-                    }
-                    if (rv.right != null) {
-                        queue.add(rv.right);
-                    }
-                }
-                if (flag && !CheckIncreasing(ll)) {
-                    return false;
-                }
-                if (!flag && !CheckDecreasing(ll)) {
-                    return false;
-                }
-                flag = !flag;
-            }
-            return true;
-        }
+/* class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        return LevelOrderTraversal(root);
     }
- 
-/* 
+
+    public boolean CheckIncreasing(List<Integer> ll) {
+        boolean flag = true;
+
+        if (ll.get(0) % 2 == 0) {
+            flag = false;
+        }
+        for (int i = 1; i < ll.size(); i++) {
+            if (ll.get(i - 1) >= ll.get(i) || ll.get(i) % 2 == 0) {
+                flag = false;
+                break;
+            }
+
+        }
+        return flag;
+    }
+
+    public boolean CheckDecreasing(List<Integer> ll) {
+        boolean flag = true;
+
+        if (ll.get(0) % 2 != 0) {
+            flag = false;
+        }
+        for (int i = 1; i < ll.size(); i++) {
+            if (ll.get(i - 1) <= ll.get(i) || ll.get(i) % 2 != 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    public boolean LevelOrderTraversal(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean flag = true;
+
+        while (!queue.isEmpty()) {
+            List<Integer> ll = new ArrayList<>();
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode rv = queue.remove();
+                ll.add(rv.val);
+                if (rv.left != null) {
+                    queue.add(rv.left);
+                }
+                if (rv.right != null) {
+                    queue.add(rv.right);
+                }
+            }
+            if (flag && !CheckIncreasing(ll)) {
+                return false;
+            }
+            if (!flag && !CheckDecreasing(ll)) {
+                return false;
+            }
+            flag = !flag;
+        }
+        return true;
+    }
+} */
+
+
 class Solution {
     public boolean isEvenOddTree(TreeNode root) {
         return LevelOrderTraversal(root);
@@ -124,4 +123,3 @@ class Solution {
         return true;
     }
 }
- */
