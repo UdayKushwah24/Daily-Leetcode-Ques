e15af001286b38004499fe1bba64be16a96e669a
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-11 12:41:33 +0530

    Time: 460 ms (91.49%) | Memory: 160.3 MB (93.46%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findMaximumXOR(int[] nums) {
+    /* public int findMaximumXOR(int[] nums) {
         if(nums[0] == 10821) return 32767;
         if(nums[0] == 832772071) return 1073741823;
         int ans = 0;
@@ -9,5 +9,75 @@ class Solution {
             }
         }
         return ans;
+    } */
+
+    static class Trie { // Binary Tree
+        class Node {
+            Node zero;
+            Node one;
+        }
+        private Node root = new Node();
+        public void add(int val) {
+            Node curr = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (val & (1 << i));
+                if (bit == 0) {
+                    if (curr.zero != null) {
+                        curr = curr.zero;
+                    } else {
+                        Node nn = new Node();
+                        curr.zero = nn;
+                        curr = nn;
+                    }
+                } else {
+                    if (curr.one != null) {
+                        curr = curr.one;
+                    } else {
+                        Node nn = new Node();
+                        curr.one = nn;
+                        curr = nn;
+                    }
+                }
+            }
+        }
+
+        public int getMaxXor(int x) {
+            int ans = 0;
+            Node curr = root;
+            for (int i = 31; i >= 0; i--) {
+                int bit = (x & (1 << i));
+                if (bit == 0) {
+                    if (curr.one != null) {
+                        ans = (ans | (1 << i));
+                        curr = curr.one;
+                    } else {
+                        curr = curr.zero;
+                    }
+                } else {
+                    if (curr.zero != null) {
+                        ans = (ans | (1 << i));
+                        curr = curr.zero;
+                    } else {
+                        curr = curr.one;
+                    }
+
+                }
+            }
+            return ans;
+        }
+
+    }
+
+    public int findMaximumXOR(int[] nums) {
+       
+        Trie tt = new Trie();
+        for (int val : nums) {
+            tt.add(val);
+        }
+        int maxXor = Integer.MIN_VALUE;
+        for (int val : nums) {
+            maxXor = Math.max(maxXor, tt.getMaxXor(val));
+        }
+        return maxXor;
     }
 }
