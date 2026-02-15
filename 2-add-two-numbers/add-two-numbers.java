9b1d99a6021efa5714c15ad35044dd177785b296
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-15 09:47:28 +0530

    Time: 1 ms (99.74%) | Memory: 46.8 MB (17.78%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pointer1 = l1;
+        ListNode pointer2 = l2;
+        ListNode res = new ListNode(-1);
+        ListNode result = res;
+        int carry = 0;
+        while(pointer1 != null || pointer2 != null) {
+            int sum = ((pointer1 != null) ? pointer1.val : 0) + ((pointer2 != null) ? pointer2.val : 0) + carry;
+            res.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            if(pointer1 != null) pointer1 = pointer1.next;
+            if(pointer2 != null) pointer2 = pointer2.next;
+
+            res = res.next;
+        }
+        if(carry > 0) {
+            res.next = new ListNode(carry);
+        }
+        return result.next;
+    }
+}
