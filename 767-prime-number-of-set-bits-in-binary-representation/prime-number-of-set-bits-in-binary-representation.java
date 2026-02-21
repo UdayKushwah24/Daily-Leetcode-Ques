535016d2bfbe862d3ae3dcf27645ec8c25f3d677
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-22 01:56:17 +0530

    Time: 5 ms (64.67%) | Memory: 42.5 MB (27.65%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
 
     public boolean CheckPrime(int n) {
         if (n == 1) {
@@ -27,6 +27,24 @@ class Solution {
             if (CheckPrime(countBit(i))) {
                 count++;
             }
+        }
+        return count;
+    }
+} */
+
+class Solution {
+
+    public boolean CheckPrime(int n) {
+        if (n == 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+    public int countPrimeSetBits(int left, int right) {
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (CheckPrime(Integer.bitCount(i))) count++;
         }
         return count;
     }
