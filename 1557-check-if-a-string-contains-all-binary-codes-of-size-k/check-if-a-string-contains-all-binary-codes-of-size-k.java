50158c69d6e761db1ee1c91680d6733c346faa49
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-23 18:30:21 +0530

    Time: 402 ms (5.19%) | Memory: 100.2 MB (8.15%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public boolean hasAllCodes(String s, int k) {
         if(k == 15 ) return true;
         if(k == 16 && s.startsWith("00000000000000001111")) return true;
@@ -22,4 +22,39 @@ class Solution {
         return true;
     }
 
+} */
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+
+        if (s.length() < k)
+            return false;
+
+        // Step 1: Store all substrings of size k
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            set.add(s.substring(i, i + k));
+        }
+
+        // Step 2: Generate all 2^k binary combinations (YOUR LOGIC)
+        for (int i = 0; i < (1 << k); i++) {
+
+            StringBuilder str = new StringBuilder();
+            int y = i;
+
+            for (int x = 0; x < k; x++) {
+                if ((y & 1) == 1)
+                    str.insert(0, "1");
+                else
+                    str.insert(0, "0");
+
+                y >>= 1;
+            }
+
+            if (!set.contains(str.toString()))
+                return false;
+        }
+
+        return true;
+    }
 }
