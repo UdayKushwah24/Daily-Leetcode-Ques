345ac675bb0bea7b87307d4d67c02330af4a536b
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-25 18:59:34 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (40.60%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1) return nums[0];
@@ -30,6 +30,41 @@ class Solution {
         int rob = nums[i] + Robber(nums, i + 2, dp);
 
         int notRob = Robber(nums, i + 1, dp);
+
+        return dp[i] = Math.max(rob, notRob);
+
+    }
+} */
+
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1) return nums[0];
+        int[] dp1 = new int[n ];
+        Arrays.fill(dp1, -1);
+        
+
+        int[] dp2 = new int[n ];
+        Arrays.fill(dp2, -1);
+       
+        int removeLast = Robber(nums, 0, n-1, dp1);
+        int removeFirst = Robber(nums, 1, n, dp2);
+        return Math.max(removeLast, removeFirst);
+    }
+
+    public static int Robber(int[] nums, int i, int n, int[] dp) {
+
+        if (i >= n) {
+            return 0;
+        }
+        if (dp[i] != -1)
+            return dp[i];
+        int rob = nums[i] + Robber(nums, i + 2,n, dp);
+
+        int notRob = Robber(nums, i + 1,n, dp);
 
         return dp[i] = Math.max(rob, notRob);
 
