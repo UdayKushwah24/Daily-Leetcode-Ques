5cd27d28e15a5924695402340a5ff886db1bd5dc
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-02-25 17:55:02 +0530

    Time: 8 ms (61.40%) | Memory: 46.2 MB (97.98%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,5 +1,5 @@
 class Solution {
-      class pair {
+    class pair {
         int val;
         int setBit;
         public pair(int val, int setBit) {
@@ -7,35 +7,32 @@ class Solution {
             this.setBit = setBit;
         }
     }
-
-    public   int CountSetBit(int n) {
+    public int CountSetBit(int n) {
         int c = 0;
-        while(n > 0) {
+        while (n > 0) {
             c++;
-            n = n & (n-1);
+            n = n & (n - 1);
         }
-
         return c;
     }
 
     public int[] sortByBits(int[] arr) {
         List<pair> ll = new ArrayList<>();
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             pair p = new pair(arr[i], CountSetBit(arr[i]));
             ll.add(p);
         }
-        Collections.sort(ll, new Comparator<pair>(){
+        Collections.sort(ll, new Comparator<pair>() {
             @Override
             public int compare(pair p1, pair p2) {
-                if(p1.setBit == p2.setBit) {
+                if (p1.setBit == p2.setBit) {
                     return p1.val - p2.val;
                 } else {
                     return p1.setBit - p2.setBit;
                 }
             }
         });
-
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             pair p = ll.get(i);
             arr[i] = p.val;
         }
