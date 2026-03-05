1631fb1168079cfe42882fee288082d73b7340c0
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-03-05 09:35:03 +0530

    Time: 1 ms (100.00%) | Memory: 43.6 MB (91.05%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -28,6 +28,8 @@
     }
 } */
 
+
+/* 
 class Solution {
     public int minOperations(String s) {
         int bit = 0;
@@ -53,6 +55,36 @@ class Solution {
             idx += 2;
         }
 
+        return bit;
+    }
+} */
+
+
+
+
+
+class Solution {
+    public int minOperations(String s) {
+        int bit = 0;
+        int a = operation(s, '0');
+        int b = operation(s, '1');
+        return Math.min(a, b);
+    }
+
+    public static int operation(String s, char ch) {
+        int bit = 0;
+        int n = s.length();
+        char ch2 = ch == '1' ? '0' : '1';
+        for (int i = 0; i < n ; ) {
+            if (i < n && s.charAt(i) != ch) {
+                bit++;
+            }
+            i++;
+            if (i < n && s.charAt(i) != ch2) {
+                bit++;
+            }
+            i++;
+        }
         return bit;
     }
 }
