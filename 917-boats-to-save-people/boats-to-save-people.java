bd45eba4a1012d2f20aa07f9f7fb60c3c8dcc8fc
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-03-14 08:50:36 +0530

    Time: 20 ms (83.14%) | Memory: 56.7 MB (34.15%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -0,0 +1,67 @@
+/* class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boat = 0;
+        PriorityQueue<Integer> large = new PriorityQueue<>();
+        PriorityQueue<Integer> small = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < people.length; i++) {
+            if (people[i ] == limit) {
+                boat++;
+            } else {
+                large.add(people[i]);
+                small.add(people[i]);
+            }
+        }
+
+        while (!large.isEmpty() && !small.isEmpty()) {
+            if (large.peek() + small.peek() == limit) {
+                boat++;
+                large.poll();
+                small.poll();
+            } else {
+                break;
+            }
+        }
+       
+        boat += large.size();
+        return boat;
+    }
+} */
+
+
+/* 
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boat = 0;
+        Arrays.sort(people);
+        int i = 0; 
+        int j = people.length-1;
+        while(i <= j) {
+            if(people[i] + people[j] <= limit) {
+                i++;
+                j--;
+            } else {
+                j--;
+            }
+            boat++;
+        }
+        return boat;
+    }
+} */
+
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int boat = 0;
+        Arrays.sort(people);
+        int i = 0; 
+        int j = people.length-1;
+        while(i <= j) {
+            if(people[i] + people[j] <= limit) {
+                i++;
+            } 
+            j--;
+            boat++;
+        }
+        return boat;
+    }
+}
