bb66dfa6f8d163a50c7ab9f382c91cc3c42241ec
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-03-26 00:27:59 +0530

    Time: 4 ms (97.20%) | Memory: 160.4 MB (80.42%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        System.out.println(grid.length); 
-        System.out.println(grid[0].length); 
-        if( grid.length == 2 && grid[0].length == 42951 ){
-            return false;
-        } 
-        // if(grid[0][0] == 100000 &&  grid.length == 2 && grid[0].length == 42951 && grid[grid.length-1][grid[0].length-1] == 100000){
-        //     return false;
-        // } 
+       
         int rows = grid.length;
         int cols = grid[0].length;
-        int[] rowSum = new int[rows];
-        int[] colSum = new int[cols];
-        int totalSum = 0;
+        //  if( rows  == 2 && cols == 42951 ){
+        //     return false;
+        // } 
+        long[] rowSum = new long[rows];
+        long[] colSum = new long[cols];
+        long totalSum = 0;
         for(int i=0; i<rows; i++){
-            int temp = 0;
+            long temp = 0;
             for(int j=0; j<cols; j++){
                 temp += grid[i][j];
             }
@@ -22,15 +18,15 @@ class Solution {
             totalSum += temp;
         }
         for(int j=0; j<cols; j++){
-            int temp = 0;
+            long temp = 0;
             for(int i=0; i<rows; i++){
                 temp += grid[i][j];
             }
             colSum[j] = temp;
         }
-        int total = totalSum;
+        long total = totalSum;
 
-        int temp = rowSum[0];
+        long temp = rowSum[0];
         for(int i=1; i<rowSum.length; i++){
             if(totalSum - rowSum[i-1] == temp){
                 return true;
@@ -38,7 +34,7 @@ class Solution {
             totalSum = totalSum - rowSum[i-1];
             temp += rowSum[i];
         }
-        int temp2 = colSum[0];
+        long temp2 = colSum[0];
         for(int i=1; i<colSum.length; i++){
             if(total - colSum[i-1] == temp2){
                 return true;
