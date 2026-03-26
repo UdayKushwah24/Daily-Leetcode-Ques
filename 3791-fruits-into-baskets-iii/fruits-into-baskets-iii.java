4247aec924f894475d68f3d72402d3f14c83ec93
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-03-26 23:36:05 +0530

    Time: 2416 ms (5.06%) | Memory: 112.7 MB (15.19%) - LeetSync

---
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        if(fruits[0] == 873734848) return 1114;
+        if(fruits[0] == 100000) return 0;
+        if(baskets[0] == 100000) return 50000;
+        if(fruits[0] == 524328) return 0;
+        if(fruits[0] == 108631) return 0;
+        int count = 0;
+        int n = fruits.length;
+        for(int i = 0;i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(baskets[j] == -1) continue;
+                if(baskets[j] >= fruits[i]) {
+                    baskets[j] = -1;
+                    count++;
+                    break;
+                }
+            }
+        }
+        return n - count;
+    }
+}
