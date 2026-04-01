e2b214b1dc4f28cf7ee1b8508a55cf6efdaa5a96
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-04-01 23:41:03 +0530

    Time: 348 ms (14.18%) | Memory: 236.7 MB (18.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,48 +1,82 @@
 class Solution {
-    /* public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-        Arrays.fill(dp, 1);
-        for(int i = 1 ;i  < n;i++) {
-            for(int j = i-1; j >= 0; j--) {
-                if(nums[j] < nums[i]) {
-                    int l = dp[j];
-                    dp[i] = Math.max(dp[i], l+1);
-                }
-            }
-        }
-        return Arrays.stream(dp).max().getAsInt();
-    } */
 
-    public int lengthOfLIS(int[] arr) {
-        List<Integer> ll = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            int idx = BinarySearch(ll, arr[i]);
-            if (idx == -1) {
-                ll.add(arr[i]);
-                System.out.println(ll);
-            } else {
-                ll.remove(idx);
-                ll.add(idx, arr[i]);
-            }
+    int arr[];
+    int dp[][];
+    int n;
+    public int solve(int i, int prev) {
+        if(i >= arr.length){
+            return 0 ;
         }
-       // System.out.println(ll);
-        return ll.size();
+        if(prev != -1 && dp[i][prev] != -1) return dp[i][prev];
+        int take = 0;
+        if(prev == -1 || arr[prev] < arr[i]) {
+            take = 1 + solve(i+1, i);
+        }
+        int skip = solve(i+1, prev);
+        if(prev != -1) dp[i][prev] =  Math.max(take, skip);
+        return Math.max(take, skip);
     }
-
-    public static int BinarySearch(List<Integer> ll, int ele) {
-        int low = 0;
-        int high = ll.size() - 1;
-        int idx = -1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (ll.get(mid) >= ele) {
-                idx = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+    public int lengthOfLIS(int[] nums) {
+        arr = nums;
+        n = arr.length;
+        dp = new int[n+1][n+1];
+        for(int a[] : dp) {
+            Arrays.fill(a, -1);
         }
-        return idx;
+        return solve(0, -1);
     }
 }
+
+
+
+
+
+
+// class Solution {
+//     /* public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         int dp[] = new int[n];
+//         Arrays.fill(dp, 1);
+//         for(int i = 1 ;i  < n;i++) {
+//             for(int j = i-1; j >= 0; j--) {
+//                 if(nums[j] < nums[i]) {
+//                     int l = dp[j];
+//                     dp[i] = Math.max(dp[i], l+1);
+//                 }
+//             }
+//         }
+//         return Arrays.stream(dp).max().getAsInt();
+//     } */
+
+//     public int lengthOfLIS(int[] arr) {
+//         List<Integer> ll = new ArrayList<>();
+//         for (int i = 0; i < arr.length; i++) {
+//             int idx = BinarySearch(ll, arr[i]);
+//             if (idx == -1) {
+//                 ll.add(arr[i]);
+//                 System.out.println(ll);
+//             } else {
+//                 ll.remove(idx);
+//                 ll.add(idx, arr[i]);
+//             }
+//         }
+//        // System.out.println(ll);
+//         return ll.size();
+//     }
+
+//     public static int BinarySearch(List<Integer> ll, int ele) {
+//         int low = 0;
+//         int high = ll.size() - 1;
+//         int idx = -1;
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (ll.get(mid) >= ele) {
+//                 idx = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return idx;
+//     }
+// }
