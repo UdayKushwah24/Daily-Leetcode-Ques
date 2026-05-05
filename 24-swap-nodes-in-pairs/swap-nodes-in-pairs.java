11af3e3426fa1c035465abddae758a5def0e23e8
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-05-06 00:17:11 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (65.96%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -14,15 +14,16 @@ class Solution {
     //     if(head.next == null) return head;
     //     ListNode ans = head.next;
 
-
     //     return ans;
     // }
 
     public ListNode swapPairs(ListNode head) {
-        if(head == null) return head;
-        if(head.next == null) return head;
+        if (head == null)
+            return head;
+        if (head.next == null)
+            return head;
         List<Integer> ll = new ArrayList<>();
-        while(head.next != null) {
+        while (head.next != null) {
             ll.add(head.val);
             head = head.next;
         }
@@ -35,22 +36,22 @@ class Solution {
         //         arr[i+1] = ll.get(i);
         //     }
         // }
-        for(int i = 0; i < n; i+=2) {
-    if(i + 1 < n) {   // ✅ FIX
-        arr[i] = ll.get(i+1);
-        arr[i+1] = ll.get(i);
-    } else {
-        arr[i] = ll.get(i);   // ✅ last element same rahega
-    }
-}
+        for (int i = 0; i < n; i += 2) {
+            if (i + 1 < n) { // ✅ FIX
+                arr[i] = ll.get(i + 1);
+                arr[i + 1] = ll.get(i);
+            } else {
+                arr[i] = ll.get(i); // ✅ last element same rahega
+            }
+        }
         // for(int i = 0; i < n ; i++) {
         //     System.out.print(arr[i] + " ");
         // }
         // System.out.println();
         // System.out.println(ll);
         ListNode temp = new ListNode(arr[0]);
-        ListNode ans = temp; 
-        for(int i = 1; i < n; i++) {
+        ListNode ans = temp;
+        for (int i = 1; i < n; i++) {
             ans.next = new ListNode(arr[i]);
             ans = ans.next;
         }
