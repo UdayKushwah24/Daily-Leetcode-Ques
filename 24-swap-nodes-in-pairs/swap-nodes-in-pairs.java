ec51f2e8dbe9dd4fb66cc4988a15fe4770968441
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-05-06 00:17:07 +0530

    Time: 1 ms (0.98%) | Memory: 43.3 MB (12.42%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // public ListNode swapPairs(ListNode head) {
+    //     if(head == null) return head;
+    //     if(head.next == null) return head;
+    //     ListNode ans = head.next;
+
+
+    //     return ans;
+    // }
+
+    public ListNode swapPairs(ListNode head) {
+        if(head == null) return head;
+        if(head.next == null) return head;
+        List<Integer> ll = new ArrayList<>();
+        while(head.next != null) {
+            ll.add(head.val);
+            head = head.next;
+        }
+        ll.add(head.val);
+        int n = ll.size();
+        int[] arr = new int[n];
+        // for(int i = 0; i < n; i+=2) {
+        //     if(i < n) {
+        //         arr[i] = ll.get(i+1);
+        //         arr[i+1] = ll.get(i);
+        //     }
+        // }
+        for(int i = 0; i < n; i+=2) {
+    if(i + 1 < n) {   // ✅ FIX
+        arr[i] = ll.get(i+1);
+        arr[i+1] = ll.get(i);
+    } else {
+        arr[i] = ll.get(i);   // ✅ last element same rahega
+    }
+}
+        // for(int i = 0; i < n ; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+        // System.out.println();
+        // System.out.println(ll);
+        ListNode temp = new ListNode(arr[0]);
+        ListNode ans = temp; 
+        for(int i = 1; i < n; i++) {
+            ans.next = new ListNode(arr[i]);
+            ans = ans.next;
+        }
+        return temp;
+    }
+}
