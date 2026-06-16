84888f2e02c3623a6dc3d186c5c5dc5135ffc909
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-06-16 16:10:41 +0530

    Time: 3 ms (100.00%) | Memory: 55.3 MB (48.53%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -1,44 +1,44 @@
-// class Solution {
-//     public String processStr(String s) {
-//         StringBuilder sb = new StringBuilder();
-//         int n = s.length();
-//         for(int i = 0; i < n; i++) {
-//             char ch = s.charAt(i);
-//             if(sb.length() > 0 && ch == '*') {
-//                 sb.deleteCharAt(sb.length()-1);
-//             } else if(ch == '#') {
-//                 sb.append(sb.toString());
-//             } else if(ch == '%') {
-//                 sb.reverse();
-//             }  else {
-//                 sb.append(ch);
-//             }
-//         }
-//         return sb.toString();
-//     }
-// }
-
 class Solution {
     public String processStr(String s) {
-        StringBuilder ans = new StringBuilder();
-
-        for (char ch : s.toCharArray()) {
-            if (Character.isLowerCase(ch)) {
-                ans.append(ch);
-            }
-            else if (ch == '*') {
-                if (ans.length() > 0) {
-                    ans.deleteCharAt(ans.length() - 1);
-                }
-            }
-            else if (ch == '#') {
-                ans.append(ans.toString());
-            }
-            else {
-                ans.reverse();
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+        for(int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if(sb.length() > 0 && ch == '*') {
+                sb.deleteCharAt(sb.length()-1);
+            } else if(ch == '#') {
+                sb.append(sb.toString());
+            } else if(ch == '%') {
+                sb.reverse();
+            }  else if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
             }
         }
-
-        return ans.toString();
+        return sb.toString();
     }
 }
+
+// class Solution {
+//     public String processStr(String s) {
+//         StringBuilder ans = new StringBuilder();
+
+//         for (char ch : s.toCharArray()) {
+//             if (Character.isLowerCase(ch)) {
+//                 ans.append(ch);
+//             }
+//             else if (ch == '*') {
+//                 if (ans.length() > 0) {
+//                     ans.deleteCharAt(ans.length() - 1);
+//                 }
+//             }
+//             else if (ch == '#') {
+//                 ans.append(ans.toString());
+//             }
+//             else {
+//                 ans.reverse();
+//             }
+//         }
+
+//         return ans.toString();
+//     }
+// }
