e7a4dd96695d5ca1550b55952c4a700fe44b0133
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-06-18 11:33:22 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (23.81%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+        double ans = 0.0;
+        double angleInOneHour = 360 / 12;
+        double totalAngleMadeByHourHand = (hour + (minutes / 60.0)) * angleInOneHour;
+        double angleMadeByMinuteHand = minutes * 6;
+        ans = Math.abs(totalAngleMadeByHourHand - angleMadeByMinuteHand);
+        if (ans < 180) {
+            return ans;
+        } else {
+            return 360 - ans;
+        }
+    }
+}
