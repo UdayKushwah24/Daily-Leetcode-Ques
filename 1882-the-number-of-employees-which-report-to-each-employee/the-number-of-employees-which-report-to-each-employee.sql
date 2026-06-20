a93856bba81be5bda9bf14408410ee53fd387bf8
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-06-20 10:58:47 +0530

    Time: 653 ms (59.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+
+-- with temp as (select * from Employees where reports_to is not null)
+-- select e.reports_to as employee_id , e.name, count(t.reports_to) as "reports_count"
+-- , avg(t.age) as average_age from Employees as e
+-- inner join temp t  on t.reports_id = e.employee_id
+-- group by t.ereports_to
+-- order by employee_id;
+
+
+WITH temp AS (
+    SELECT * 
+    FROM Employees 
+    WHERE reports_to IS NOT NULL
+)
+SELECT 
+    e.employee_id, 
+    e.name, 
+    COUNT(t.employee_id) AS reports_count, 
+    ROUND(AVG(t.age)) AS average_age
+FROM Employees AS e
+INNER JOIN temp AS t 
+    ON e.employee_id = t.reports_to
+GROUP BY e.employee_id 
+ORDER BY e.employee_id;
