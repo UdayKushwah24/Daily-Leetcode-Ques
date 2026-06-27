4729546da7bb91f73826e4e8e9e0661324ed806e
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-06-27 06:07:06 +0530

    Time: 101 ms (29.07%) | Memory: 67.3 MB (59.30%) - LeetSync

--- 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
+++ 3299-find-the-maximum-number-of-elements-in-subset/find-the-maximum-number-of-elements-in-subset.java
@@ -1,45 +1,48 @@
 class Solution {
-   public int maximumLength(int[] nums) {
-	HashMap<Integer, Integer> map = new HashMap<>();
-	ArrayList<Integer> list = new ArrayList<>();
+    public int maximumLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
-	for(int num : nums){
-		map.put(num, map.getOrDefault(num, 0)+1);
-		if(map.get(num) == 1) list.add(num);
-	}
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.get(num) == 1)
+                list.add(num);
+        }
 
-	Collections.sort(list);
-	int i=0;
+        Collections.sort(list);
+        int i = 0;
 
-	Set<Integer> used = new HashSet<>();
-	int ct = 1;
+        Set<Integer> used = new HashSet<>();
+        int ct = 1;
 
-	// for pattern like - [1], [1,1,1], [1,1,1,1,1], etc...
-	if(map.containsKey(1)){
-		int val = map.get(1);
-		if(val%2 == 0) val--;
-		ct = val;
-		used.add(1);
-	}
+        // for pattern like - [1], [1,1,1], [1,1,1,1,1], etc...
+        if (map.containsKey(1)) {
+            int val = map.get(1);
+            if (val % 2 == 0)
+                val--;
+            ct = val;
+            used.add(1);
+        }
 
-	while(i < list.size()){
-		int top = list.get(i++);
-		if(!used.add(top)) continue;
+        while (i < list.size()) {
+            int top = list.get(i++);
+            if (!used.add(top))
+                continue;
 
-		int curr = 0;
-		while(map.getOrDefault(top, 0) > 1) {
-			used.add(top);
-			top *= top;
-			curr += 2;
-		}
+            int curr = 0;
+            while (map.getOrDefault(top, 0) > 1) {
+                used.add(top);
+                top *= top;
+                curr += 2;
+            }
 
-		if(map.getOrDefault(top, 0) == 1){
-			curr+=2;
-		}
+            if (map.getOrDefault(top, 0) == 1) {
+                curr += 2;
+            }
 
-		ct = Math.max(curr-1, ct);
-	}
+            ct = Math.max(curr - 1, ct);
+        }
 
-	return ct;
-}
+        return ct;
+    }
 }
