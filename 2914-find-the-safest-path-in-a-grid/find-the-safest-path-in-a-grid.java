a6139076afc9bda18bd6a1be47cce065153259eb
Uday Kushwah <142593408+UdayKushwah24@users.noreply.github.com>
2026-07-01 07:04:18 +0530

    Time: 164 ms (38.73%) | Memory: 121.4 MB (46.35%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.java
@@ -1,17 +1,12 @@
 class Solution {
-
     static int[] roww = {0, 0, 1, -1};
     static int[] coll = {1, -1, 0, 0};
-
     public int maximumSafenessFactor(List<List<Integer>> grid) {
         int row = grid.size(), col = grid.get(0).size();
         if (grid.get(0).get(0) == 1 || grid.get(row - 1).get(col - 1) == 1) return 0;
-
         int[][] distance = new int[row][col];
         for (int[] d : distance) Arrays.fill(d, 9999);
-
         dist(distance, grid);
-
         int l = 0, r = distance[0][0];
         while (l <= r) {
             int mid = (l + r) / 2;
@@ -20,10 +15,8 @@ class Solution {
         }
         return l - 1;
     }
-
-    static void dist(int[][] distance, List<List<Integer>> grid) {
+    public static void dist(int[][] distance, List<List<Integer>> grid) {
         Queue<int[]> q = new LinkedList<>();
-
         for (int i = 0; i < distance.length; i++) {
             for (int j = 0; j < distance[0].length; j++) {
                 if (grid.get(i).get(j) == 1) {
@@ -32,11 +25,9 @@ class Solution {
                 }
             }
         }
-
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             int r = cur[0], c = cur[1];
-
             for (int i = 0; i < 4; i++) {
                 int nr = r + roww[i], nc = c + coll[i];
                 if (nr >= 0 && nc >= 0 && nr < distance.length && nc < distance[0].length && distance[nr][nc] > distance[r][c] + 1) {
@@ -46,13 +37,11 @@ class Solution {
             }
         }
     }
-
     static boolean find_val(int target, int[][] distance) {
         int[][] vis = new int[distance.length][distance[0].length];
         Queue<int[]> q = new LinkedList<>();
         vis[0][0] = 1;
         q.add(new int[]{0, 0});
-
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             int r = cur[0], c = cur[1];
